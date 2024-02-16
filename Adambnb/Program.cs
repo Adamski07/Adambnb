b7273805f97ce8e9951735ad3ed8c0c6cdd30b22
Adamski07 <s1178952@student.windesheim.nl>
2024-02-16 12:17:58 +0100

    cors toegevoegd

--- Adambnb/Program.cs
+++ Adambnb/Program.cs
@@ -7,6 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+               builder =>
+               {
+                   builder.WithOrigins("https://cloudbnb-df3c1.web.app")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+               });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 
